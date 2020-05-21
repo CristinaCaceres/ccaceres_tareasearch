048660a7e6a60cfa25a4a7264f8b5aa4bd715aac
CCaceres <cristina.caceres.jauregui@gmail.com>
2020-05-21 18:00:30 -0500

    Tarea busqueda de peliculas

---
+++ 03-searchpeliculas.js
@@ -0,0 +1,214 @@
+const dibujarPeliculas = ({ results }) => {
+  results.forEach((peli) => {
+
+    let carouselCell = $(`<div class="carousel-cell"></div>`);
+    let card = $(`<div class="card"></div>`);
+    let imagen = $(`<div class="card-img-top imagen-pelicula"></div>`);
+    imagen.css("background-image", `url("https://image.tmdb.org/t/p/w500${peli.poster_path}")`);
+
+    let cardBody = $(`<div class="card-body"></div>`);
+    let cardTitle = $(`<h4 class="card-title">${peli.title}</h4>`);
+
+    let cardText = $(`<p class="card-text"></p>`);
+    let spanTexto = $(`<span>${peli.overview.substr(0, 50)}</span>`);
+    let leerMas = $(`<span class="text-danger">... Leer Mas</span>`);
+
+    cardText.append(spanTexto);
+    cardText.append(leerMas);
+
+    leerMas.click(() => {
+      // capturamis nuevamente al slider de flickity
+      let flick = $('#contenedor-peliculas').flickity({
+        // options
+        cellAlign: 'left',
+        contain: true,
+      });
+
+      let leerMasTexto = leerMas.text();
+      if (leerMasTexto.indexOf("Mas") > -1) {
+        // texto estaba chiquito
+        spanTexto.text(peli.overview);
+        leerMas.text(" Leer Menos");
+        // recalculamos la altura de flickity
+        flick.flickity('resize')
+      } else {
+        // texto estaba largo
+        spanTexto.text(peli.overview.substr(0, 50));
+        leerMas.text("... Leer Mas");
+        // recalculamos la altura de flickity
+        flick.flickity('resize')
+      }
+    })
+
+
+    cardBody.append(cardTitle);
+    cardBody.append(cardText);
+    card.append(imagen);
+    card.append(cardBody);
+
+    carouselCell.append(card);
+
+    $("#contenedor-peliculas").append(carouselCell);
+  });
+
+  // ocultando alerta
+  $("#alertCargando").attr("hidden", "hidden")
+  // mostrando el carousel
+  $("#contenedor-peliculas").removeAttr("hidden");
+
+  // Toma al slider de Flickity y y lo convierte en UN SLIDER como tal
+  $('#contenedor-peliculas').flickity({
+    // options
+    cellAlign: 'left',
+    contain: true,
+  });
+}
+
+const traerPeliculas = () => {
+  let endpoint = "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=105eb79aa1e6df60a2b95878ad2289aa&language=es";
+  fetch(endpoint, { method: 'GET', body: null })
+    .then((response) => {
+      response.json().then((data) => {
+        dibujarPeliculas(data);
+      })
+    })
+}
+
+traerPeliculas();
+
+$("#busqueda").click(e => {
+  let textoBuscado = $("#textoBuscado").val();
+  textoBuscado = (textoBuscado.trim()).replace(" ", "%20");
+
+  if (textoBuscado === "") {
+    return false;
+  }
+  else {
+    let endpoint = `https://api.themoviedb.org/3/search/movie?api_key=105eb79aa1e6df60a2b95878ad2289aa&language=es&query=${textoBuscado}`;
+    console.log(endpoint);
+    console.log($("#contenedor-peliculas"));
+    $("#contenedor-peliculas").empty();
+    console.log($("#contenedor-peliculas"));
+
+    fetch(endpoint, { method: 'GET', body: null })
+      .then((response) => {
+        response.json().then((data) => {
+          filtrarPeliculas(data);
+        })
+      })
+
+    // para que no se recargue la pagina 
+    return false;
+
+  }
+
+
+});
+
+
+const filtrarPeliculas = ({ results }) => {
+
+  results.forEach((peli) => {
+    console.log(peli);
+
+    let carouselCell = $(`<div class="carousel-cell"></div>`);
+    let card = $(`<div class="card"></div>`);
+    let imagen = $(`<div class="card-img-top imagen-pelicula"></div>`);
+
+    if (peli.poster_path === null) {
+      imagen.css("background-image", `url("imagen_null.jpg")`);
+    }
+    else {
+      imagen.css("background-image", `url("https://image.tmdb.org/t/p/w500${peli.poster_path}")`);
+    }
+
+    let cardHeader = $(`<div class="card-header"></div>`);
+    let cardTitle = $(`<h4 class="card-title">${peli.title}</h4>`);
+
+
+    let cardBody = $(`<div class="card-body"></div>`);
+
+    let cardTextRelease = $(`<p class="card-text text-right"></p>`);
+    let smallTextRelease = $(`<small class="text-muted">Fecha de Lanzamiento: ${peli.release_date}</small>`);
+
+    cardTextRelease.append(smallTextRelease);
+
+    let cardText = $(`<p class="card-text"></p>`);
+    let spanTexto = $(`<span>${peli.overview.substr(0, 50)}</span>`);
+    let leerMas = $(`<span class="text-danger">... Leer Mas</span>`);
+
+    cardText.append(spanTexto);
+    cardText.append(leerMas);
+
+    leerMas.click(() => {
+      // capturamis nuevamente al slider de flickity
+      let flick = $('#contenedor-peliculas').flickity({
+        // options
+        cellAlign: 'left',
+        contain: true,
+      });
+
+      let leerMasTexto = leerMas.text();
+      if (leerMasTexto.indexOf("Mas") > -1) {
+        // texto estaba chiquito
+        spanTexto.text(peli.overview);
+        leerMas.text(" Leer Menos");
+        // recalculamos la altura de flickity
+        flick.flickity('resize')
+      } else {
+        // texto estaba largo
+        spanTexto.text(peli.overview.substr(0, 50));
+        leerMas.text("... Leer Mas");
+        // recalculamos la altura de flickity
+        flick.flickity('resize')
+      }
+    })
+
+    let cardFooter = $(`<div class="card-footer text-muted text-center"></div>`);
+    let cardTextFooter = $(`<p class="card-text text-center"></p>`);
+    let iTextFooter = $(`<i class="fas fa-star"></i> ${peli.popularity}
+                         <i class="fa fa-thumbs-up"></i> ${peli.vote_average}
+                         <i class="fa fa-thumbs-up"></i> ${peli.vote_count} `);
+
+    cardTextFooter.append(iTextFooter);
+
+
+    cardHeader.append(cardTitle);
+    cardBody.append(cardTextRelease);
+    cardBody.append(cardText);
+    cardFooter.append(cardTextFooter);
+
+    card.append(imagen);
+    card.append(cardHeader);
+    card.append(cardBody);
+    card.append(cardFooter);
+
+    carouselCell.append(card);
+
+    $("#contenedor-peliculas").append(carouselCell);
+  });
+
+
+  // ocultando alerta
+  $("#alertCargando").attr("hidden", "hidden")
+  // mostrando el carousel
+  $("#contenedor-peliculas").removeAttr("hidden");
+
+  // Toma al slider de Flickity y y lo convierte en UN SLIDER como tal
+  $('#contenedor-peliculas').flickity({
+    // options
+    cellAlign: 'left',
+    contain: true,
+  });
+
+}
+
+/*
+                                                      <div class="card-footer text-muted text-center">
+                                                            <p class="card-text text-center">
+
+                                                            </p>
+
+                                                      </div>
+
+*/
